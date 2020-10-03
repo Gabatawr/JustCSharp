8712589c06c780ddf48fb44173d5264b8f8205c6
Gabatawr <gabatawr@gmail.com>
2020-10-04 00:12:16 +0300

    fix

--- Week 2/Meeting 5/Program.cs
+++ Week 2/Meeting 5/Program.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Globalization;
 using System.Threading;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
 
 using Angar;
 
@@ -22,18 +17,18 @@ namespace Meeting_5
 {
     class Program
     {
-        static void Mix(ref Tank[] arr)
-        {
-            Random rand = new Random();
-            for (int i = arr.Length - 1; i >= 1; i--)
-            {
-                int j = rand.Next(i + 1);
+        //static void Mix(ref Tank[] arr)
+        //{
+        //    Random rand = new Random();
+        //    for (int i = arr.Length - 1; i >= 1; i--)
+        //    {
+        //        int j = rand.Next(i + 1);
 
-                Tank temp = (Tank)arr[j]?.Clone();
-                arr[j] = (Tank)arr[i]?.Clone();
-                arr[i] = (Tank)temp?.Clone();
-            }
-        }
+        //        Tank temp = (Tank)arr[j]?.Clone();
+        //        arr[j] = (Tank)arr[i]?.Clone();
+        //        arr[i] = (Tank)temp?.Clone();
+        //    }
+        //}
         static void CursorStartLine()
         {
             int top = Console.CursorTop;
@@ -89,27 +84,20 @@ namespace Meeting_5
                             int result = winners[k] * array[j][i]; // Бой
 
                             Console.Write(winners[k] + "\n" + array[j][i]);
-                            Thread.Sleep(900);
-                            CursorStartLine();
-                            Thread.Sleep(300);
+                            Thread.Sleep(900); CursorStartLine(); Thread.Sleep(300);
 
                             Console.Write(winners[k] + "\n" + array[j][i]);
-                            Thread.Sleep(600);
-                            CursorStartLine();
-                            Thread.Sleep(200);
+                            Thread.Sleep(600); CursorStartLine(); Thread.Sleep(200);
 
                             Console.Write(winners[k] + "\n" + array[j][i]);
-                            Thread.Sleep(300);
-                            CursorStartLine();
-                            Thread.Sleep(100);
-
+                            Thread.Sleep(300); CursorStartLine(); Thread.Sleep(100);
 
                             if (result != 0)
                             {
                                 if (result > 0)
                                 {
-                                    Console.ForegroundColor = ConsoleColor.Red; Console.Write(winners[k]);
-                                    Console.WriteLine();
+                                    Console.ForegroundColor = ConsoleColor.Red;   Console.Write(winners[k]);
+                                                                                  Console.WriteLine();
                                     Console.ForegroundColor = ConsoleColor.Green; Console.Write(array[j][i]);
                                     Console.ForegroundColor = ConsoleColor.White;
 
